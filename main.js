9416ed7e989cdc8d507e581a6228ddf5abe9a891
Khj3m <109201777+lolvuongong@users.noreply.github.com>
2022-12-14 22:12:03 +0700

    hbs_js_1

---
+++ main.js
@@ -0,0 +1,19 @@
+var express = require('express')
+var app = express()
+
+app.use(express.static('public'))
+
+app.set('view engine', 'hbs')
+app.use(express.urlencoded({extended:true}))
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.get('/search', (req, res) => {
+    
+}) 
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT)
+console.log("Loopian project is runnning at port 5000!!!")
